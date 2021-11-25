b704019fe0a0eb3402292bbf05953bb2a7fa34df
Harry Marr <harry.marr@gmail.com>
2021-11-25 12:20:21 -0500

    Improve JS screen view perf by preallocating

--- hack-web/www/screen-view.ts
+++ hack-web/www/screen-view.ts
@@ -4,6 +4,8 @@ export class ScreenView {
   el: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
   emulator: HackEmulator;
+  pixelBuffer: Uint8ClampedArray;
+  imageData: ImageData;
 
   constructor(emulator: HackEmulator) {
     this.emulator = emulator;
@@ -13,7 +15,13 @@ export class ScreenView {
     this.el.style.width = '512px';
     this.el.style.height = '770px';
     this.ctx = this.el.getContext('2d')!;
-    // this.ctx.scale(2, 2);
+
+    const memorySize = 0x6000;
+    const screenHeight = memorySize / 32;
+    const arrayBuffer = new ArrayBuffer(512 * screenHeight * 4);
+    this.pixelBuffer = new Uint8ClampedArray(arrayBuffer);
+    this.imageData = new ImageData(this.pixelBuffer, 512, screenHeight);
+
 
     this.update();
   }
@@ -21,47 +29,24 @@ export class ScreenView {
   update() {
     this.drawMemory(this.emulator.memory.slice(0x0000, 0x4000), 0);
     this.drawMemory(this.emulator.memory.slice(0x4000, 0x6000), 514);
-    // this.ctx.fillStyle = 'rgb(200, 200, 200)';
-    // this.ctx.clearRect(0, 0, 512, 768);
-
-    // const arrayBuffer = new ArrayBuffer(512 * 768 * 4);
-    // const pixels = new Uint8ClampedArray(arrayBuffer);
-
-    // this.emulator.memory.slice(0x0000, 0x6000).forEach((n, wordIndex) => {
-    //   for (let bitIndex = 0; bitIndex < 16; bitIndex++) {
-    //     const pixelIndex = (wordIndex * 16 + bitIndex) * 4;
-    //     pixels[pixelIndex] = 0;
-    //     if ((n & (1 << bitIndex)) >> bitIndex !== 0) {
-    //       pixels[pixelIndex + 1] = 255;
-    //     } else {
-    //       pixels[pixelIndex + 1] = 0;
-    //     }
-    //     pixels[pixelIndex + 2] = 0;
-    //     pixels[pixelIndex + 3] = 255;
-    //   }
-    // });
   }
 
   drawMemory(memory: Uint16Array, yOffset: number) {
-    const height = memory.length / 32;
-    const arrayBuffer = new ArrayBuffer(512 * height * 4);
-    const pixels = new Uint8ClampedArray(arrayBuffer);
-
-    memory.forEach((n, wordIndex) => {
+    for (let wordIndex = 0; wordIndex < memory.length; wordIndex++) {
+      const n = memory[wordIndex];
       for (let bitIndex = 0; bitIndex < 16; bitIndex++) {
         const pixelIndex = (wordIndex * 16 + bitIndex) * 4;
-        pixels[pixelIndex] = 0;
+        this.pixelBuffer[pixelIndex] = 0;
         if ((n & (1 << bitIndex)) >> bitIndex !== 0) {
-          pixels[pixelIndex + 1] = 255;
+          this.pixelBuffer[pixelIndex + 1] = 255;
         } else {
-          pixels[pixelIndex + 1] = 0;
+          this.pixelBuffer[pixelIndex + 1] = 0;
         }
-        pixels[pixelIndex + 2] = 0;
-        pixels[pixelIndex + 3] = 255;
+        this.pixelBuffer[pixelIndex + 2] = 0;
+        this.pixelBuffer[pixelIndex + 3] = 255;
       }
-    });
+    }
 
-    const imageData = new ImageData(pixels, 512, height);
-    this.ctx.putImageData(imageData, 0, yOffset)
+    this.ctx.putImageData(this.imageData, 0, yOffset)
   }
 }
