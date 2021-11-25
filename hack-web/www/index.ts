cc3bb6a194b041131eb4044145a39baefc1f6210
Harry Marr <harry.marr@gmail.com>
2021-11-25 16:13:21 -0500

    Hide debug info by default

--- hack-web/www/index.ts
+++ hack-web/www/index.ts
@@ -6,15 +6,18 @@ import { RomLoader } from './rom-loader';
 
 class App {
   emulator: HackEmulator;
+  debugMode: boolean;
   cpuView: CpuView;
   screenView: ScreenView;
   perfView: PerfView;
   running: boolean;
   lastFrameTime: number;
   startBtn: HTMLButtonElement;
+  stepBtn: HTMLButtonElement;
 
   constructor() {
     this.emulator = new HackEmulator();
+    this.debugMode = false;
     this.running = false;
     this.lastFrameTime = performance.now();
 
@@ -53,24 +56,26 @@ class App {
     });
     controlsEl.append(this.startBtn);
 
-    const stepBtn = document.createElement('button');
-    stepBtn.innerText = 'Step';
-    stepBtn.disabled = true;
-    stepBtn.addEventListener('click', () => this.update(1));
-    controlsEl.append(stepBtn);
+    this.stepBtn = document.createElement('button');
+    this.stepBtn.innerText = 'Step';
+    this.stepBtn.disabled = true;
+    this.stepBtn.addEventListener('click', () => this.update(1));
+    controlsEl.append(this.stepBtn);
 
     const romLoader = new RomLoader(rom => {
       this.emulator.load_rom(rom);
       this.cpuView.update();
       this.screenView.update();
       this.startBtn.disabled = false;
-      stepBtn.disabled = false;
+      this.stepBtn.disabled = false;
     });
     controlsEl.append(romLoader.el);
 
     diagsEl.append(controlsEl);
 
     document.body.append(appEl);
+
+    this.toggleDebugMode(false);
   }
 
   start() {
@@ -93,17 +98,28 @@ class App {
     }
     const stepTime = performance.now() - t1;
 
-    this.cpuView.update();
+    if (this.debugMode) {
+      this.cpuView.update();
+    }
+
     this.screenView.update();
 
-    const frameTime = performance.now() - this.lastFrameTime;
-    this.lastFrameTime = performance.now();
-
-    this.perfView.update(stepTime, frameTime);
+    if (this.debugMode) {
+      const frameTime = performance.now() - this.lastFrameTime;
+      this.lastFrameTime = performance.now();
+      this.perfView.update(stepTime, frameTime);
+    }
 
     if (this.running) {
       requestAnimationFrame(this.update.bind(this, steps));
     }
+  }
+
+  toggleDebugMode(status: boolean) {
+    this.debugMode = status;
+    this.cpuView.el.style.display = status ? '' : 'none';
+    this.perfView.el.style.display = status ? '' : 'none';
+    this.stepBtn.style.display = status ? '' : 'none';
   }
 }
 
