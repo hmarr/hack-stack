4b1b0bbd478f443797c7b4b904ab18354558195f
Harry Marr <harry.marr@gmail.com>
2023-04-14 22:01:18 -0400

    hack-web: clippy

--- hack-web/src/lib.rs
+++ hack-web/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::new_without_default)]
+
 mod panic_handler;
 
 use hack_stack::emulator;
@@ -31,7 +33,7 @@ impl HackEmulator {
     #[wasm_bindgen]
     pub fn load_rom(&mut self, source: &str) -> Result<(), JsValue> {
         let mut rom = Vec::<u16>::with_capacity(0x2000);
-        for line in source.lines().filter(|l| l.len() > 0) {
+        for line in source.lines().filter(|l| !l.is_empty()) {
             rom.push(
                 u16::from_str_radix(line.trim_end(), 2)
                     .map_err(|_| format!("error parsing instruction {}", line))?,
@@ -70,7 +72,7 @@ impl HackEmulator {
     pub fn screen_image_data(&mut self) -> js_sys::Uint8ClampedArray {
         for (i, word) in self.emu.memory()[0x4000..0x6000].iter().enumerate() {
             for bit_index in 0..16 {
-                let pixel_index = ((i * 16 + bit_index) * 4) as usize;
+                let pixel_index = (i * 16 + bit_index) * 4;
                 self.pixel_buffer[pixel_index] = 0;
                 self.pixel_buffer[pixel_index + 1] =
                     if (word >> bit_index) & 1 == 0 { 0 } else { 255 };
