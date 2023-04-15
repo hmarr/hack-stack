f33086e62e310f223e31960faaf15be92a29f552
Harry Marr <harry.marr@gmail.com>
2023-04-14 22:07:37 -0400

    hack-web: add snek and snek2 roms

--- hack-web/www/rom-loader.ts
+++ hack-web/www/rom-loader.ts
@@ -1,4 +1,12 @@
 import pong from './roms/pong.hack';
+import snek from './roms/snek.hack';
+import snek2 from './roms/snek2.hack';
+
+const ROMS: { [k: string]: string } = {
+  Snek: snek,
+  Snek2: snek2,
+  Pong: pong,
+};
 
 export class RomLoader {
   el: HTMLElement;
@@ -7,16 +15,19 @@ export class RomLoader {
     this.el = document.createElement('div');
 
     const select = document.createElement('select');
-    const option = document.createElement('option');
-    option.value = 'pong';
-    option.innerText = 'Pong';
-    select.append(option);
+
+    for (const name of Object.keys(ROMS)) {
+      const option = document.createElement('option');
+      option.value = name;
+      option.innerText = name;
+      select.append(option);
+    }
     this.el.append(select);
 
     const loadBtn = document.createElement('button');
     loadBtn.innerText = 'Load ROM';
     loadBtn.addEventListener('click', async () => {
-      const rsp = await fetch(pong);
+      const rsp = await fetch(ROMS[select.value]);
       onLoad(await rsp.text());
     });
     this.el.append(loadBtn);
